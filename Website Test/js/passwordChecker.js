5e65686d28b77098485730878a110f778c42a9bb
Anjali Bhardwaj <30476016+Anjalibhardwaj1@users.noreply.github.com>
2021-03-16 17:29:11 -0400

    Delete passwordChecker.js

--- Website Test/js/passwordChecker.js
+++
@@ -1,7 +0,0 @@
-
-document.getElementById("second").onblur = checkPasswords; 
-document.getElementById("myForm").onsubmit = checkPasswords;
-
-document.getElementById("second").addEventListener ("blur", checkPasswords); 
-document.getElementById("myForm").addEventListener("submit", checkPasswords);
-
