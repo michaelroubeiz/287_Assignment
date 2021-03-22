a5172d97f3e04c527d2d8813893ae8e9ffb45bda
Anjali Bhardwaj <30476016+Anjalibhardwaj1@users.noreply.github.com>
2021-03-22 10:33:37 -0400

    Delete P5P6_loginChecker.js

--- Website Test/js/P5P6_loginChecker.js
+++
@@ -1,5 +0,0 @@
-// on sumbit validate login form
-document.getElementById("myForm").onsubmit = formValidation;
-
-// on reset , reset boarder colors in login form
-document.getElementById("reset").onclick = resetInputs;
