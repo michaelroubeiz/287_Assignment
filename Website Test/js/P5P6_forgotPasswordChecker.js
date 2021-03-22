03c631bb1a50ebec766aeedc4ee5df30a0a16a39
Anjali Bhardwaj <30476016+Anjalibhardwaj1@users.noreply.github.com>
2021-03-22 10:33:42 -0400

    Delete P5P6_forgotPasswordChecker.js

--- Website Test/js/P5P6_forgotPasswordChecker.js
+++
@@ -1,5 +0,0 @@
-// on sumbit validate forgot password form
-document.getElementById("myForm").onsubmit = formValidation;
-
-// on reset , reset boarder colors
-document.getElementById("reset").onclick = resetInputs;
