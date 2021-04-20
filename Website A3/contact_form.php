78ca0ad7ba8db6483b78b613cd67e04a32fd7da7
MomooDiane <78440330+MomooDiane@users.noreply.github.com>
2021-04-20 13:36:16 +0000

    Update contact_form.php

--- Website A3/contact_form.php
+++ Website A3/contact_form.php
@@ -7,14 +7,35 @@
 <link rel="stylesheet" type="text/css" href="css/contact_us.css">
 
 <?php
-            echo $_POST["UserName"].", <b>Your request has been submitted</b> ". "<br />"; 
-            echo "Request sent for the following item: ". $_POST["ItemName"]. "<br />";
-            echo "Your Order Number is: ". $_POST["OrderNumber"]. "<br />";
-            echo "Specifications: ". $_POST["questions?"];
-      
-           
+if(!empty($_POST["UserName"]) ){
+    
+
+    $found = false;
+    $UserName = $_POST["UserName"];
+    //$email = $_POST["email"];
+    echo $UserName;
+    $file = fopen("Sign_Up_Information.txt", "r");
+}
+if($file)
+{
+    echo "File open";
+    while(($line = fgets($file)))
+    {
+       
+        $array = explode(":", $line);
+        
+        if($array[0] == $UserName){
+        
+        header("Location:P11.php ");
+        }
+        else {
+        header("Location:P6.php");
+        }
+        }
+}     
     ?>
 <br />
 <br />
 </body>
+</html>
 
