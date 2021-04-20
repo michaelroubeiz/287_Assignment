14679e39c6d6506e5e3fd1ed05ab38a5b784a99d
wardasalem <79183620+wardasalem@users.noreply.github.com>
2021-04-20 10:45:46 -0400

    Update P9.php

--- Website A3/P9/P9.php
+++ Website A3/P9/P9.php
@@ -24,7 +24,7 @@ $output = explode(PHP_EOL, $page);
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-    <link rel="stylesheet" href="css/P9_P10.css">
+    <link rel="stylesheet" href="css/P9P10.css">
 
 </head>
 
