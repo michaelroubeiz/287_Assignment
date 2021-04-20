c6f5b3f97250c937b7ba4408ef90fe0eb6319f59
mikeflof <35436139+mikeflof@users.noreply.github.com>
2021-04-20 09:58:23 -0400

    Update P8Add.php

--- Website A3/P8Add.php
+++ Website A3/P8Add.php
@@ -4,55 +4,34 @@ global $i;
 $thisPage = basename($_SERVER["PHP_SELF"]);
 include "header_P7.php";
 
-if (array_key_exists("submit", $_POST)) {
+
+if (array_key_exists("submit", $_POST) && validate()) {
     setValues();
 }
 function setValues()
 {
-    if (isset($_POST["submit"]) && !empty($_POST["submit"])) {
-        $_SESSION["product"] = $_POST["product"];
-        $_SESSION["image"] = $_POST["image"];
-        $_SESSION["quantity"] = $_POST["quantity"];
-        $_SESSION["size"] = $_POST["size"];
-        $_SESSION["type"] = $_POST["type"];
-        $_SESSION["nutritional_source"] = $_POST["nutritional_source"];
-        $_SESSION["available"] = $_POST["available"];
-        $_SESSION["price"] = $_POST["price"];
-        $_SESSION["submit"] = $_POST["submit"];
-        header("Location: P7.php");
-        exit();
-    } else {
-        echo "error";
-    }
+    $_POST["image"] = "images\\" . $_POST["image"];
+    $added = array(array($_POST["image"], $_POST["product"], $_POST["price"], $_POST["weight"], 0, $_POST["quantity"], $_POST["size"], $_POST["type"], $_POST["nutritional_source"]));
+    array_splice($_SESSION["products"][0], 6, 0,  $added);  //fix this
+    header("Location: P7.php");
+    exit();
 }
 
 if (isset($_POST["cancel"])) {
     header("Location: P7.php");
     exit();
 }
-
+function validate()
+{
+    if (empty($_POST["image"]) || empty($_POST["product"]) || empty($_POST["quantity"]) || empty($_POST["size"]) || empty($_POST["type"]) || empty($_POST["nutritional_source"]) || empty($_POST["price"] || empty($_POST["weight"]))) {
+        echo "<script>alert('Not all boxes filled.');</script>";
+        return false;
+    } else {
+        return true;
+    }
+}
 
 ?>
-
-<script type="text/javascript">
-    function validate() {
-        var product = document.getElementsByName("product").value;
-        var image = document.getElementsByName("image").value;
-        var quantity = document.getElementsByName("quantity").value;
-        var size = document.getElementsByName("size").value;
-        var type = document.getElementsByName("type").value;
-        var nutritional_source = document.getElementsByName("nutritional_source").value;
-        var available = document.getElementsByName("available").value;
-        var price = document.formsgetElementsByName("price").value;
-        if ((product == null || product == "") && (image == null || image == "") && (quantity == null || quantity == "") && (size == null || size == "") && (type == null || type == "") && (nutritional_source == null || nutritional_source == "") && (available == null || available == "") && (price == null || price == "")) {
-            alert("Not all boxes filled!");
-            return false;
-        }
-    }
-</script>
-
-
-</head>
 
 <body>
     <form class="form-horizontal" name="Form" method="POST">
@@ -64,18 +43,18 @@ if (isset($_POST["cancel"])) {
         </div>
         <div class="form-group">
             <label for="inputName3" class="col-sm-2 control-label">
-                <h5>Product:</h5>
-            </label>
-            <div class="col-sm-10">
-                <input name="product" type="text" class="form-control" id="inputName3" placeholder="ex: Banana" />
-            </div>
-        </div>
-        <div class="form-group">
-            <label for="inputName3" class="col-sm-2 control-label">
                 <h5>Select Image<span style="font-size:15px"> (png or jpg)</span>:</h5>
             </label>
             <div class="col-sm-10">
                 <input name="image" type="file" class="form-control" accept="image/png, image/jpeg" id="inputName3" placeholder="jpeg and png only" />
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="inputName3" class="col-sm-2 control-label">
+                <h5>Product:</h5>
+            </label>
+            <div class="col-sm-10">
+                <input name="product" type="text" class="form-control" id="inputName3" placeholder="ex: Banana" />
             </div>
         </div>
 
@@ -115,19 +94,17 @@ if (isset($_POST["cancel"])) {
 
         <div class="form-group">
             <label for="inputQuantity3" class="col-sm-2 control-label">
-                <h5>Quantity <span style="font-size:15px">(# Available)</span>:</h5>
-            </label>
-            <div class="col-sm-10">
-                <input name="available" type="text" class="form-control" id="inputQuantity3" placeholder="ex: 5" />
-            </div>
-        </div>
-
-        <div class="form-group">
-            <label for="inputQuantity3" class="col-sm-2 control-label">
                 <h5>Price:</h5>
             </label>
             <div class="col-sm-10">
-                <input name="price" type="text" class="form-control" id="inputPrice3" placeholder="ex: $0.33/190g" />
+                <input name="price" type="text" class="form-control" id="inputPrice3" placeholder="ex: $0.33" />
+            </div>
+            <br />
+            <label for="inputQuantity3" class="col-sm-2 control-label">
+                <h5>Weight per $:</h5>
+            </label>
+            <div class="col-sm-10">
+                <input name="weight" type="text" class="form-control" id="inputPrice3" placeholder="ex: 190g" />
             </div>
         </div>
 
